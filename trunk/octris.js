35d3498aa7a6015d45312b7b600c899baa13fa97
davidsj <davidsj@9709496c-216e-2bcb-5109-cdc634aea0c3>
2011-10-06 21:32:33 +0000

    client-side rendering of blocks

---
+++ trunk/octris.js
@@ -0,0 +1,43 @@
+blocks = [['white', null, null, null],
+          [null, 'white', null, null],
+          [null, null, 'white', null],
+          [null, null, null, 'white'],
+          [null, null, null, 'white'],
+          [null, null, null, 'white'],
+          [null, null, null, 'white'],
+          [null, null, null, 'white'],
+          [null, 'white', null, 'white']];
+
+function draw() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    width = canvas.width;
+    height = canvas.height;
+
+    padding = 80;
+    gameWidth = blocks[0].length;
+    gameHeight = blocks.length;
+
+    squareSize = Math.min((width - padding*2)/gameWidth, (height - padding*2)/gameHeight);
+    gameTop = height - padding - squareSize * gameHeight;
+    gameLeft = (width - squareSize * gameWidth) / 2;
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(gameLeft, gameTop, squareSize*gameWidth, squareSize*gameHeight);
+
+    for (y = 0; y < gameHeight; y++) {
+        for (x = 0; x < gameWidth; x++) {
+            if ((color = blocks[y][x]) != null) {
+                ctx.fillStyle = blocks[y][x];
+                ctx.fillRect(gameLeft + squareSize*x, gameTop + squareSize*y,
+                             squareSize, squareSize);
+                
+                ctx.strokeStyle = 'gray';
+                ctx.strokeRect(gameLeft + squareSize*x, gameTop + squareSize*y,
+                               squareSize, squareSize);
+            }
+        }
+    }
+}
