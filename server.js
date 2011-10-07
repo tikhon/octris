d5deccc315edec110633792e33fe945c4291d5b6
davidsj <davidsj@9709496c-216e-2bcb-5109-cdc634aea0c3>
2011-10-07 09:24:09 +0000

    two players (but incomplete, and a few bugs)

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-var websock = require('websock'); // npm install websock
+var websock = require('websock');
 
-var w = 10,
+var w = 20,
     h = 20;
+
+var game = newGame();
+var playerSockets = [null, null];
+var sockets = [];
+
+websock.listen(8888, function(socket) {
+    var player = null;
+    if (playerSockets[0] == null || playerSockets[1] == null) {
+        player = playerSockets[0] == null ? 0 : 1;
+        playerSockets[player] = socket;
+        socket.on('message', function(message) {
+            userInput(player, JSON.parse(message));
+        });
+    }
+    socket.on('close', function() {
+        if (player != null) {
+            playerSockets[player] = null;
+        }
+        var index = sockets.indexOf(socket);
+        sockets = sockets.slice(0, index).concat(sockets.slice(index+1));
+        drawGame();
+    });
+    sockets.push(socket);
+    drawGame();
+});
+
+advanceGame();
 
 function newGame() {
     var frozen = new Array();
@@ -12,8 +39,9 @@ function newGame() {
         }
     }
     
-    return {'frozen': frozen, 'falling': new Array()};
+    return {'frozen': frozen, 'falling': [newPiece(0), newPiece(1)]};
 }
+
 
 function pieceLocs(piece) {
     var locs = new Array();
@@ -25,7 +53,7 @@ function pieceLocs(piece) {
     return locs;
 }
 
-function gameColors(game) {
+function gameColors() {
     var colors = new Array();
 
     for (var y in game['frozen']) {
@@ -45,20 +73,19 @@ function gameColors(game) {
     return colors;
 }
 
-function maybeAddPiece(game) {
-    if (game['falling'].length == 0) {
-        var center = [Math.floor(w/2), 0];
-        game['falling'][0] = {'color': 'blue',
-                              'center': center,
-                              'blocks': [[-1, 0], [0, 0], [1, 0], [1, -1]]};
-    }
+function newPiece(player) {
+    var center = [Math.floor(w*(player+1)/3), 0];
+    return {'color': ['red', 'blue'][player],
+            'center': center,
+            'blocks': [[-1, 0], [0, 0], [1, 0], [1, -1]]}
 }
 
-function tryMove(game, piece, dx, dy) {
+function tryMove(piece, dx, dy) {
     var success = true;
 
     var locs = pieceLocs(piece);
     for (var j in locs) {
+        // TODO: check for collisions with other pieces
         var x = locs[j][0] + dx;
         var y = locs[j][1] + dy;
         if (x < 0 || x >= w ||
@@ -72,14 +99,11 @@ function tryMove(game, piece, dx, dy) {
     return true;
 }
 
-function fallOrFreeze(game) {
-    var newFalling = new Array();
-
+function fallOrFreeze() {
     for (var i in game['falling']) {
         var falling = game['falling'][i];
-        if (tryMove(game, falling, 0, +1)) {
-            newFalling.push(falling);
-        } else {
+        if (!tryMove(falling, 0, +1)) {
+            // TODO: only freeze if hit frozen block, not other piece
             var locs = pieceLocs(falling);
             for (var j in locs) {
                 var x = locs[j][0];
@@ -89,38 +113,48 @@ function fallOrFreeze(game) {
                 }
                 game['frozen'][y][x] = falling['color'];
             }
+            
+            // TODO: possibly destroy row
+            game['falling'][i] = newPiece(Number(i));
         }
     }
-
-    game['falling'] = newFalling;
 }
 
-function advanceGame(socket, game) {
-    fallOrFreeze(game);
-    maybeAddPiece(game);
-    socket.send(JSON.stringify(gameColors(game)));
-    
-    setTimeout(advanceGame, 250, socket, game);
+function gamePaused() {
+    return playerSockets.indexOf(null) != -1;
 }
 
-function userInput(socket, game, input) {
-    var cr = String.fromCharCode(input);
-    switch (cr) {
-    case 'j':
-        tryMove(game, game['falling'][0], -1, 0);
-        break;
-    case 'l':
-        tryMove(game, game['falling'][0], +1, 0);
-        break;
+function advanceGame() {
+    if (!gamePaused()) {
+        fallOrFreeze();
     }
-
-    socket.send(JSON.stringify(gameColors(game)));
+    
+    drawGame();
+    setTimeout(advanceGame, 300);
 }
 
-websock.listen(8888, function(socket) {
-    var game = newGame();
-    advanceGame(socket, game);
-    socket.on('message', function(message) {
-        userInput(socket, game, JSON.parse(message));
-    });
-});
+function drawGame() {
+    for (var i in sockets) {
+        sockets[i].send(JSON.stringify(gameColors()));
+    }
+}
+
+function userInput(player, input) {
+    if (!gamePaused()) {
+        var cr = String.fromCharCode(input);
+        switch (cr) {
+        case 'j':
+            tryMove(game['falling'][player], -1, 0);
+            break;
+        case 'l':
+            tryMove(game['falling'][player], +1, 0);
+            break;
+        case ' ':
+            tryMove(game['falling'][player], 0, +1);
+            break;
+        }
+
+        drawGame();
+    }
+}
+
